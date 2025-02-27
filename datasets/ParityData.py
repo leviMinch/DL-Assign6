2509633c51c2f33d25bc865776c4d668dd080e70
Levi <113947938+leviMinch@users.noreply.github.com>
2025-02-27 01:55:47 -0800

    pushing before bed, bugs galore

--- datasets/ParityData.py
+++ datasets/ParityData.py
@@ -19,7 +19,7 @@ class Parity(Dataset):
         self.data = torch.FloatTensor(list(itertools.product([0,1], repeat=max_length)))
       else:
         self.data = torch.randint(low=0, high=2, size=(samples,max_length)).to(dtype=torch.float32)
-        
+
 
     def __len__(self):
         return len(self.data)
@@ -30,20 +30,39 @@ class Parity(Dataset):
            x = self.data[idx][:l]
         else:
            x = self.data[idx]
-   
+
         y = x.sum() % 2
-        return x,y 
+        return x,y
 
 
     # Function to enable batch loader to stack binary strings of different lengths and pad them
     @staticmethod
     def pad_collate(batch):
-        
-        ###########################################
-        #
-        # Q1 TODO
-        #
-        ###########################################
+      print("Batch Shape:", batch)
+      # Get x and y value with the highest length
+      x_max = 0
+      y_max = 0
+      for sample in batch:
+        if len(batch.x) > x_max:
+          x_max = len(batch.x)
+        if len(batch.y) > y_max:
+          y_max = len(batch.y)
+
+
+      # Extract x sequences and y labels
+      # torch.cat: https://pytorch.org/docs/main/generated/torch.cat.html
+      x_seqs = torch.empty((0,x_max))
+      yy = torch.empty((0,y_max))
+      x_lens = torch.tensor((0,1))
+      for sample in batch:
+        x_seqs.cat(sample.x)
+        yy.cat(sample.y)
+        x_lens.cat(len(sample.x))
+
+      # pad xx
+      xx = pad_sequence(x_seqs, batch_first=True, padding_value=0)
+
+      return xx, yy, x_lens
 
 
 def getParityDataloader(training=True, max_length=4, batch_size=1000):
--- models/ParityLSTM.py
+++ models/ParityLSTM.py
@@ -10,7 +10,8 @@ class ParityLSTM(nn.Module) :
         # Q2 TODO
         #
         ###########################################
-
+        self.lstm = nn.LSTM(input_size=1, hidden_size=hidden_dim)
+        self.linear = nn.Linear(in_features=16, out_features=2)
 
     def forward(self, x, x_lens):
         ###########################################
@@ -18,3 +19,11 @@ class ParityLSTM(nn.Module) :
         # Q2 TODO
         #
         ###########################################
+        x = self.lstm(x)
+
+        vals = x[torch.arange(x.shape[0]), x_lens - 1, :]
+
+        out = self.linear(vals)
+
+        return out
+
